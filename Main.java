53d3545eae9f3b5e3cc85eb36134c4a1014bd507
gbsouzza <gabriel012felix@gmail.com>
2025-09-02 21:15:01 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = sc.nextInt();
+
+        int soma = num1 + num2;
+
+        System.out.println("A soma é: " + soma);
+
+        sc.close();
+    }
+}
